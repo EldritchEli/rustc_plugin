5a41ed83376741863767a4e16113a9c91f9a1ca4
Eli <eli@Elis-MacBook-Pro.local>
2026-02-24 10:19:44 +0100

    updated build

--- crates/rustc_plugin/build.rs
+++ crates/rustc_plugin/build.rs
@@ -1,7 +1,20 @@
+const DEFAULT_CHANNEL: &str = "nightly-2025-08-20";
+
 fn main() {
   let toolchain_toml = include_str!("rust-toolchain.toml");
-  let toolchain_table = toolchain_toml.parse::<toml::Table>().unwrap();
-  let toolchain = toolchain_table["toolchain"].as_table().unwrap();
-  let channel = toolchain["channel"].as_str().unwrap();
+  let Ok(toolchain_table) = toolchain_toml.parse::<toml::Table>() else {
+    log::error!("couldn't parse toolchain_toml, falling back to {DEFAULT_CHANNEL}");
+    println!("cargo:rustc-env=RUSTC_CHANNEL={DEFAULT_CHANNEL}");
+    return;
+  };
+
+  let toolchain = toolchain_table["toolchain"]
+    .as_table()
+    .expect("expected rust-toolchain.toml to contain a valid toolchain")
+    .clone();
+  let channel = toolchain["channel"]
+    .as_str()
+    .expect("rust-toolchain.toml should contain channel field");
+
   println!("cargo:rustc-env=RUSTC_CHANNEL={channel}");
 }
